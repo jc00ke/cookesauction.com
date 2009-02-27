30bdeb812016256d3aedea3fb5076fc410e01d37
Jesse Cooke <jesse@chino.(none)>
2009-02-26 19:42:03 -0800

    Removing unneeded files and updating cms-ish functionality.

--- site/site.rb
+++ site/site.rb
@@ -1,5 +1,15 @@
 %w[rubygems sinatra].each { |r| require r }
 
 get '/' do
-	"sup?"
+	haml :index
+end
+
+get '/master.css' do
+  header 'Content-Type' => 'text/css; charset=utf-8'
+  sass :master  
+end
+
+
+get '/:page' do
+  haml params[:page].intern
 end
