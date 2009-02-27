c12c587f5af59c6ac146bfe77948ffb6e8f66323
Jesse Cooke <jesse@chino.(none)>
2009-02-26 19:56:27 -0800

    Adding listing model

--- site/site.rb
+++ site/site.rb
@@ -1,11 +1,40 @@
-%w[rubygems sinatra].each { |r| require r }
+%w[rubygems sinatra dm-core dm-validations dm-timestamps].each { |r| require r }
+
+
+
+
+class Listing
+  include DataMapper::Resource
+  
+  property :id, Serial
+
+  property :city, String, :nullable => false
+  property :result, String
+  property :zip, String, :nullable => false
+  property :title, String, :nullable => false
+  property :number_photos, Integer, :default => 0
+  property :id, Serial
+  property :street_address, String, :nullable => false
+  property :type, Enum[:public_auction, :real_estate], :default => :public_auction
+  property :content, Text, :nullable => false
+  property :created_at, DateTime
+  property :updated_at, DateTime
+  property :starting_at, DateTime
+  property :visible, Boolean, :default => false
+  property :update, Text
+  property :state, String, :nullable => false
+
+end
+
+
+
 
 get '/' do
 	haml :index
 end
 
 get '/master.css' do
-  header 'Content-Type' => 'text/css; charset=utf-8'
+  content_type 'text/css', :charset => 'utf-8'
   sass :master  
 end
 
