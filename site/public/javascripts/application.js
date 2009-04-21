7b1c93faca542f8aa45dc93e580d209540bdb4dd
Jesse Cooke <jesse@chino.(none)>
2009-04-20 20:40:44 -0700

    Moving to a more simple site. Testimonials are back as content. No more ajax email signup.

--- site/public/javascripts/application.js
+++ site/public/javascripts/application.js
@@ -1,45 +1,5 @@
 // Common JavaScript code across your application goes here.
 
-var destroyDialog = function() { $('#dialog').dialog('destroy'); };
-
 $(document).ready(function() {
-  
-  $('#email_signup a').click(function(e) {
-    e.preventDefault();
-    $('#dialog').dialog({
-      title: 'Email List',
-      autoOpen: true,
-      width: 330,
-      height: 300,
-      draggable: false,
-      modal: true,
-      resizable: false,
-      open: function() {
-        $('#dialog').load('/signup form');
-      },
-      close: function() {
-        destroyDialog();
-      },
-      buttons: {
-        'Signup': function() {
-          $.ajax({
-            type: "POST",
-            url: "/signup",
-            data: { name: $('#name').val(), your_email: $('#your_email').val() },
-            success: function(thanks){
-              $('#dialog').html(thanks);
-              setTimeout(function() { destroyDialog() }, 1500 );
-            },
-            error: function(oops) {
-              $('#dialog').html(oops);
-            }
-          });
-        },
-        'Cancel': function() {
-          destroyDialog();
-        }
-      }
-    });
-  });
   
 });
--- site/site.rb
+++ site/site.rb
@@ -1,8 +1,6 @@
 require 'rubygems'
-$:.unshift File.dirname(__FILE__) + '/lib'
-
 require 'sinatra'
-require 'partials'
+require 'lib/partials'
 require 'dm-core'
 require 'dm-validations'
 require 'dm-timestamps'
@@ -20,7 +18,6 @@ class Listing
   property :zip, String, :nullable => false
   property :title, String, :nullable => false
   property :number_photos, Integer, :default => 0
-  property :id, Serial
   property :street_address, String, :nullable => false
   property :type, Enum[:public_auction, :real_estate], :default => :public_auction
   property :content, Text, :nullable => false
@@ -45,16 +42,6 @@ class Submission
   
 end
 
-class Testimonial
-  include DataMapper::Resource
-  
-  property :id, Serial
-  property :name, String, :nullable => false
-  property :origin, String, :nullable => false
-  property :body, Text, :nullable => false
-  
-end
-
 class Email
   include DataMapper::Resource
   
@@ -65,16 +52,6 @@ class Email
   property :email, String, :format => :email_address, :nullable => false
   
 end
-
-=begin
-@@testimonials = []
-@@testimonials << Testimonial.new('Anonymous', "Your web site is great; it keeps me up to date with all the upcoming auctions. Your auctioneer team is one of the best I've seen so far. All the items I've acquired, I got at an unbeatable price and in excellent condition. My wife and I enjoy attending your auctions. Thanks, and keep up the good work!")
-@@testimonials << Testimonial.new('Anonymous', "Your web site is as fine as any I visit. I only get to the Midwest on rare occasion, but like to take in an auction when there. Thanks and keep up the good work.")
-@@testimonials << Testimonial.new('Kevin Filko', "I have never submitted a testimonial about an auction company before, but I am so impressed with these people and the way they run their auctions that i just have to let you know what a pleasure it was doing business with them. Their auctions are always on time, the articles for sale are exactly as described, and the best part of all THEY'RE A LOT OF FUN! Thanks for a wonderful experience from start to finish; I can't wait till your next auction! Keep up the good work!")
-@@testimonials << Testimonial.new('Tony Mier', "Cooke's is one of the most honest, professional and reputable auctions I have dealt with and I have been extremely pleased! I do long distance business with some of the finest and most prestigious auction houses in the US and have business accounts with many of them. I would urge all to have extreme confidence in all dealings with them!")
-@@testimonials << Testimonial.new('Anthony Santolino', "As a former worker for Cooke's, I must say he is one of the most professional businessmen I have met! He's a relentless worker and most of all cares about the buyers needs. In today's market you just don't get that kind of passion and caring anymore! Great job Rick for running an honest business!")
-@@testimonials << Testimonial.new('Steven Reinhart', "As a former employee and associate of Cooke's, I can honestly remark that the services provided to the public are top-drawer. Furthermore, Rick is one of the finest auctioneers in the area and has a reputation for putting the customer's best interest at the forefront of his operation and business dealings.")
-=end
 
 ## CONFIGURATION ###########################
 configure :development do
@@ -130,7 +107,7 @@ post '/signup' do
     haml :thanks, :layout => !request.xhr?
   else
     #Don't like this.
-    haml :not_acceptable, :layout => !request.xhr?
+    haml :error, :layout => !request.xhr?
   end
 end
 
@@ -143,13 +120,6 @@ end
 post '/unsubscribe' do
   @title, @body_id = prep 'unsubscribe'
   haml :bye, :layout => !request.xhr?
-end
-
-## TESTIMONIALS ###########################
-get '/testimonials' do
-  @title, @body_id = prep 'testimonials'
-  @testimonials = Testimonial.all.reverse
-  haml :testimonials
 end
 
 
@@ -171,9 +141,13 @@ not_found do
   @body_id = 'not_found'
   haml :not_found
 end
-=begin
+
 error do
   @title = 'Hmm, something broke...'
   @body_id = 'error'
-  haml :not_acceptable
-=end
+  haml :error
+end
+
+get '/admin/testimonials' do
+  
+end
