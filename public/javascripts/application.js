6ccd7c8c09be33f075e5c9af469817f801e01b31
jc00ke <jesse@jc00ke.com>
2011-03-19 15:39:10 -0700

    Add lazy loading of images

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,4 +4,9 @@ $(document).ready(function() {
   $('#listing_photos a').fancybox();
 
   $("a[href^='http']").not('rel="photos"').attr('target', '_blank');
+
+  $("#listing_photos img").lazyload({
+    effect: "fadeIn",
+    placeHolder: "/fancybox/blank.gif"
+   });
 });
