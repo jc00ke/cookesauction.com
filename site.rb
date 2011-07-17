2caf0c4fca8537d5f0063ad46c4edb035b871460
jc00ke <jesse@jc00ke.com>
2011-07-17 13:10:18 -0700

    Switch to MongoLab

--- site.rb
+++ site.rb
@@ -12,12 +12,12 @@ require 'pony'
 
 # MongoDB configuration
 Mongoid.configure do |config|
-  if ENV['MONGOHQ_URL']
-    conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
-    uri = URI.parse(ENV['MONGOHQ_URL'])
+  if ENV['MONGOLAB_URL']
+    conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URL'])
+    uri = URI.parse(ENV['MONGOLAB_URL'])
     config.master = conn.db(uri.path.gsub(/^\//, ''))
   else
-    config.master = Mongo::Connection.from_uri("mongodb://localhost:27017").db('cookes')
+    config.master = Mongo::Connection.from_uri("mongodb://localhost:27017").db('cookesauction')
   end
 end
 
