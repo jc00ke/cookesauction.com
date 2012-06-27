e5a2dda60b5b996bbd2cc1a716ee35f90085a00c
jc00ke <jesse@jc00ke.com>
2012-06-27 10:01:38 -0700

    Force us-east-1 region for uploads

--- lib/tasks/images.rb
+++ lib/tasks/images.rb
@@ -62,7 +62,8 @@ class Images < Thor
     connection = Fog::Storage.new({
       :provider => 'AWS',
       :aws_access_key_id => ENV['S3_ACCESS_KEY_ID'],
-      :aws_secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
+      :aws_secret_access_key => ENV['S3_SECRET_ACCESS_KEY'],
+      :region => 'us-east-1'
     })
 
     directory = connection.directories.create(
