c28563fa0b28a2ed2b4ba23d255d50b3d3d117d5
jc00ke <jesse@jc00ke.com>
2015-05-30 16:24:05 -0700

    Update fancybox to v2
    Also update jQuery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,8 +3,6 @@
 $(document).ready(function() {
   $('#listing_photos a').fancybox();
 
-  $("a[href^='http']").not('rel="photos"').attr('target', '_blank');
-
   $("#listing_photos img").lazyload({
     effect: "fadeIn",
     placeHolder: "/fancybox/blank.gif"
