af68dc75b3088cb6eb28f7c412a1b267998abc0a
jc00ke <jesse@jc00ke.com>
2015-10-04 12:40:21 -0700

    Use start/end of string not line regex delimiters

--- lib/models.rb
+++ lib/models.rb
@@ -2,7 +2,7 @@ require 'mongoid'
 
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), 'models')
 
-EMAIL_REGEXP = /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+EMAIL_REGEXP = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 require 'page'
 require 'listing'
 require 'email'
