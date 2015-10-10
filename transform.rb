4545489464946043f04391f7580578205edbdd1a
jc00ke <jesse@jc00ke.com>
2015-10-10 12:50:55 -0700

    Add new listings data file

--- transform.rb
+++ transform.rb
@@ -1,11 +1,9 @@
 require "json"
-require "time"
 
 json = File.open("data/old_listings.json")
 listings = JSON.load(json)
 
 new_listings = listings.map { |listing|
-  t = Time.parse(listing["starting_at"])
   {
     "slug" => listing["slug"],
     "street_address" => listing["street_address"],
@@ -13,9 +11,8 @@ new_listings = listings.map { |listing|
     "state" => listing["state"],
     "zip" => listing["zip"],
     "location" => listing["location"],
-    "number_photos" => listing["number_photos"],
-    "date" => t.strftime("%Y-%m-%d"),
-    "time" => t.strftime("%H:%M"),
+    "number_photos" => listing["number_photos"].to_i,
+    "starting_at" => listing["starting_at"],
     "type" => listing["sale_type"],
     "title" => listing["sale_title"],
     # derive page
@@ -27,7 +24,9 @@ new_listings = listings.map { |listing|
     "result" => listing["result"],
     "update_text" => listing["updated_text"],
   }
-}
+}.sort_by { |listing|
+  listing["starting_at"]
+}.reverse
 
 File.open("data/listings.json", "wb") do |file|
   file.puts JSON.pretty_generate(new_listings)
