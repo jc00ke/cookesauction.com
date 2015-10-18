4262de08a92b0d1280ea8fc093a10fef03b920d9
jc00ke <jesse@jc00ke.com>
2015-10-18 14:30:17 -0700

    starting_at -> date time

--- transform.rb
+++ transform.rb
@@ -1,18 +1,28 @@
 require "json"
+require "time"
 
 json = File.open("data/old_listings.json")
 listings = JSON.load(json)
 
 new_listings = listings.map { |listing|
+  t = Time.parse(listing["starting_at"])
+  date = "#{t.year}-#{t.month}-#{t.day}"
+
+  if [1, 2].include?(t.hour) && t.min == 45
+    time = "9:45"
+  else
+    min = t.min.zero? ? "00" : t.min
+    time = "#{t.hour}:#{min}"
+  end
   {
     "slug" => listing["slug"],
+    "starting_at" => "#{date} #{time}",
     "street_address" => listing["street_address"],
     "city" => listing["city"].strip,
     "state" => listing["state"],
     "zip" => listing["zip"],
     "location" => listing["location"],
     "number_photos" => listing["number_photos"].to_i,
-    "starting_at" => listing["starting_at"],
     "type" => listing["sale_type"],
     "title" => listing["sale_title"],
     # derive page
@@ -25,8 +35,8 @@ new_listings = listings.map { |listing|
     "update_text" => listing["updated_text"],
   }
 }.sort_by { |listing|
-  listing["starting_at"]
-}.reverse
+  Time.parse(listing["starting_at"]).to_i * -1
+}
 
 File.open("data/listings.json", "wb") do |file|
   file.puts JSON.pretty_generate(new_listings)
