a3e06c5d8634070de89936d0cd215c430b263899
jc00ke <jesse@jc00ke.com>
2015-10-18 22:42:27 -0700

    Remove old, unused models

--- lib/models.rb
+++
@@ -1,9 +0,0 @@
-require 'mongoid'
-
-$LOAD_PATH.unshift File.join(File.dirname(__FILE__), 'models')
-
-EMAIL_REGEXP = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-require 'page'
-require 'listing'
-require 'email'
-require 'submission'
