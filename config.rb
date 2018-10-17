0069754577790071115c3e420020aa29e8654ac6
Jesse Cooke <jesse@jc00ke.com>
2018-10-17 16:59:45 -0700

    Switch to com.cookesauction

--- config.rb
+++ config.rb
@@ -49,7 +49,7 @@ activate :pagination do
 end
 
 activate :s3_sync do |s3|
-  s3.bucket = "cookesauction.com"
+  s3.bucket = "com.cookesauction"
   s3.region = "us-west-2"
   s3.aws_access_key_id = ENV["AWS_ACCESS_KEY_ID"]
   s3.aws_secret_access_key = ENV["AWS_SECRET_ACCESS_KEY"]
