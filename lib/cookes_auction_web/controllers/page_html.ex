739294afbf1670453f814e6733c287c804cb9bd6
Jesse Cooke <jesse@relativepath.io>
2024-10-29 20:48:05 -0700

    Initial commit of Phoenix w/sqlite

---
+++ lib/cookes_auction_web/controllers/page_html.ex
@@ -0,0 +1,10 @@
+defmodule CookesAuctionWeb.PageHTML do
+  @moduledoc """
+  This module contains pages rendered by PageController.
+
+  See the `page_html` directory for all templates available.
+  """
+  use CookesAuctionWeb, :html
+
+  embed_templates "page_html/*"
+end
